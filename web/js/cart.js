30f471fdb220e7e9fa5f745a269b68cecdddd0e2
Nha Le <thanhnha0706@gmail.com>
2017-05-27 20:24:57 +0700

    Fixed add book to cart
    Fixed cart in header section
    Fixed cart page
    Cart now show correct number of items and price

--- web/js/cart.js
+++ web/js/cart.js
@@ -1,14 +1,44 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 $(document).ready(function () {
     $.get("cart?action=get", function (data, status) {
-//        console.log(data[0]);
-        for (var i = 0; i < data.length; i++) {
-            $('#cart').append('<tr bgcolor="#FFFFFF" class=" product-detail"><td valign="top"><img src="images/image07.jpg" /></td><td valign="top">' + data[i].book.title +'</td><td align="center" valign="top">' + data[i].book.price + '</td><td align="center" valign="top">1</td><td align="center" valign="top">' + data[i].book.price + '</td><td align="center" valign="top"><a href="#"><i class="icon-trash"></i></a></td></tr>');
+
+        // check whether data is empty
+        if (!_.isEmpty(data)) {
+
+            $cartHeaderButton = $("#cartHeaderButton");
+            $cart = $("#cart");
+            $cartGrandTotal = $("#cartGrandTotal");
+
+            // group item by bookId
+            var items = _.groupBy(data, "book.bookId");
+            var itemCount = _.size(items);
+            var total = _.sumBy(data, "book.price");
+
+            // update the cart information in header
+            // check whether it exists
+            if ($cartHeaderButton.length) {
+                $cartHeaderButton.html(itemCount + ' item(s) - ' + total + '₫<span class="caret"></span>');
+            }
+
+            // if in the cart page
+            if ($cart.length) {
+
+                // update the cart detail
+                _.forEach(items, function (books, key) {
+
+                    // these books are identical
+                    var quantity = books.length;
+                    var book = books[0].book;
+                    var subTotal = book.price * quantity;
+                   
+                    $cart.append('<tr bgcolor="#FFFFFF" class=" product-detail"><td valign="top"><img src="images/image07.jpg" /></td><td valign="top">' + book.title + '</td><td align="center" valign="top">' + book.price + '₫</td><td align="center" valign="top">' + quantity + '</td><td align="center" valign="top">' + subTotal + '₫</td><td align="center" valign="top"><a href="#"><i class="icon-trash"></i></a></td></tr>');
+
+                });
+                
+                // update the cart grand total
+                $cartGrandTotal.html(total + "₫");
+            }
         }
+
     });
 });
